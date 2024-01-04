5520d400bb5957ef751c4ecd98a957275decf776
mohammadzaki18 <mohammadzakijack@gmail.com>
2024-01-04 23:24:50 +0700

    COM

--- pages/Segmentation Def.py
+++ pages/Segmentation Def.py
@@ -7,9 +7,6 @@ st.header('Apa itu Segmentation?')
 st.write("""
  Segmentation sendiri merupakan proses dimana bertujuan untuk memisahkan struktur anatomi yang berbeda dalam gambar MRI, 
 dalam penelitian ini kami memisahkan struktur tersebut yang dinamakan "Condyle" yang terdapat pada rahang manusia. 
-Segmentasi Condyle sendiri sangat membantu para ahli medis untuk mengidentifikasi dan menganalisis struktur anatomi 
-yang berkaitan dengan penyakit atau kondisi pasien. Hal ini membantu para ahli medis dalam mengidentifikasi masalah kesehatan, merumuskan pengobatan, 
-         dan memantau pengembangan penyakit pasien.
 
 Berikut adalah contoh segmentasi yang kami dapatkan:
 """)
@@ -21,3 +18,16 @@ with col1:
 
 with col2:
     st.image('img_sources\Condyle Area.png', caption="Setelah Disegmentasikan", use_column_width=True)
+
+
+st.header('Methodology')
+
+st.write("""
+ Segmentation sendiri merupakan proses dimana bertujuan untuk memisahkan struktur anatomi yang berbeda dalam gambar MRI, 
+dalam penelitian ini kami memisahkan struktur tersebut yang dinamakan "Condyle" yang terdapat pada rahang manusia. 
+Segmentasi Condyle sendiri sangat membantu para ahli medis untuk mengidentifikasi dan menganalisis struktur anatomi 
+yang berkaitan dengan penyakit atau kondisi pasien. Hal ini membantu para ahli medis dalam mengidentifikasi masalah kesehatan, merumuskan pengobatan, 
+         dan memantau pengembangan penyakit pasien.
+
+Berikut adalah contoh segmentasi yang kami dapatkan:
+""")
